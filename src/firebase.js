4b0ab664087f8d223559c56189ac6a1c9d8f3590
viren sureja <virensureja3@gmail.com>
2021-04-07 16:22:53 +0530

    folder setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDo-ji2gAT5lyOPXQuzFI38SHgTybMg9-A',
+	authDomain: 'clone-netflix-v2.firebaseapp.com',
+	projectId: 'clone-netflix-v2',
+	storageBucket: 'clone-netflix-v2.appspot.com',
+	messagingSenderId: '288691901300',
+	appId: '1:288691901300:web:99df90bc72f90da77a5221',
+	measurementId: 'G-7RGMK0CMFC',
+};
