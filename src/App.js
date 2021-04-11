35c3634a7d7250dd0e9c9cb7b763e673da3b8e26
viren sureja <virensureja3@gmail.com>
2021-04-11 23:04:13 +0530

    new routes as well as login/logout update fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
-	let user = null;
+	const user = useSelector(selectUser);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const unSubscribe = auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {
+				// login
+				dispatch(
+					login({
+						uid: userAuth.uid,
+						email: userAuth.email,
+					})
+				);
+			} else {
+				// logout
+				dispatch(logout());
+			}
+		});
+		return unSubscribe;
+	}, [dispatch]);
+
 	return (
 		<div className="App">
 			<Router>
@@ -13,6 +37,9 @@ function App() {
 					<LoginScreen />
 				) : (
 					<Switch>
+						<Route path="/profile">
+							<ProfileScreen />
+						</Route>
 						<Route exact path="/">
 							<HomeScreen />
 						</Route>
