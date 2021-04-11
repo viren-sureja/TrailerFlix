02be33fac6d5bcc7d56af9513bdd1d67ba2d7311
viren sureja <virensureja3@gmail.com>
2021-04-11 09:25:47 +0530

    firebase auth and db created

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyDo-ji2gAT5lyOPXQuzFI38SHgTybMg9-A',
 	authDomain: 'clone-netflix-v2.firebaseapp.com',
@@ -8,3 +11,10 @@ const firebaseConfig = {
 	appId: '1:288691901300:web:99df90bc72f90da77a5221',
 	measurementId: 'G-7RGMK0CMFC',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
