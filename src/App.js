6e00b7af955744295fb9dfbba5aebacf3e95a6b0
viren sureja <virensureja3@gmail.com>
2021-04-11 09:26:16 +0530

    new route added for signIn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
+	let user = null;
 	return (
 		<div className="App">
-			{/* homescreen */}
-			<HomeScreen />
-			{/*  */}
-			{/*  */}
+			<Router>
+				{!user ? (
+					<LoginScreen />
+				) : (
+					<Switch>
+						<Route exact path="/">
+							<HomeScreen />
+						</Route>
+						<Route path="/how">
+							<h1>hello</h1>
+						</Route>
+					</Switch>
+				)}
+			</Router>
 		</div>
 	);
 }
